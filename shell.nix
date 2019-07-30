545076cb3ea71f82cb33d8d63a380adb1ddf3996
Arian van Putten <arian.vanputten@gmail.com>
2019-07-30 14:18:16 +0200

    Make shell.nix work! It's a miracle!

--- shell.nix
+++ shell.nix
@@ -1,10 +1,5 @@
-let
-  channels_ = builtins.fromJSON (builtins.readFile ./nixpkgs.json);
-  channels  = builtins.mapAttrs (k: v: import (builtins.fetchGit v) {
-  }) channels_;
-  pkgs = channels."nixos-19.03";
-in
-  pkgs.mkShell {
-    name = "shell";
-    buildInputs = [ pkgs.go ];
-  }
+(import ./default.nix).overrideAttrs (_: {
+  shellHook = ''
+     eval "$configurePhase"
+  '';
+})
