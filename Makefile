f1dc30d49f007d120cee480a973aafe08c9933f4
Arian van Putten <arian.vanputten@gmail.com>
2019-08-20 19:53:40 +0200

    add an install phase.
    still need to figure out how to make nix actually call the makefile

--- Makefile
+++ Makefile
@@ -3,3 +3,7 @@ plugins/nomad-systemd-driver:
 	go build -o plugins/nomad-systemd-driver
 
 
+install:
+	mkdir -p ${out}
+	cp -r plugins ${out}
+
--- default.nix
+++ default.nix
@@ -10,6 +10,7 @@ in
     # modSha256 = pkgs.lib.fakeSha256;
     modSha256 = "0c2p3315pi19nqirhljq95sl6h4kc6np0dsrzbx417l5nnvflz51";
 
+
     # modSha256 = "04c5ck3x8g11d66c9v55n4b0yybm6vfwpxa2p7fiqlniy30hs17c";
     # dunno
     buildInputs = with pkgs; [ nomad vault consul ];
